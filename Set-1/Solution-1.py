f1bc59fd18085acd76cfb0697ff9d72c20c1b201
Nupur Agrawal <nupur202000@gmail.com>
2020-04-11 01:36:17 +0530

    Adds solution-1

---
+++ Set-1/Solution-1.py
@@ -0,0 +1,4 @@
+from binascii import unhexlify, b2a_base64
+import base64
+
+print(base64.b64encode(unhexlify("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))
