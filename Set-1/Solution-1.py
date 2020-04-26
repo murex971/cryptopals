31de3712cb43c72208b436b5b88ce1d18ca5745a
Nupur Agrawal <nupur202000@gmail.com>
2020-04-26 21:59:33 +0530

    Edits solution-1

--- Set-1/Solution-1.py
+++ Set-1/Solution-1.py
@@ -1,4 +1,4 @@
-from binascii import unhexlify, b2a_base64
-import base64
+from binascii import unhexlify, hexlify
+from base64 import b64encode, b64decode
 
-print(base64.b64encode(unhexlify("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))
+print(b64encode(unhexlify("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))
