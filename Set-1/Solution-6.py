7d6af12a3da267f6feb363dda431ba743c392ef2
Nupur Agrawal <nupur202000@gmail.com>
2020-04-27 20:16:25 +0530

    Adds function to determine key size

--- Set-1/Solution-6.py
+++ Set-1/Solution-6.py
@@ -1,4 +1,5 @@
 from binascii import unhexlify,hexlify
+from base64 import b64encode, b64decode
 
 def XORfunc(a, b):
     output = ""
@@ -7,7 +8,22 @@ def XORfunc(a, b):
     return output
 
 def HammingDistance(str1, str2):
+    # conversion to binary form.
     result_string = ''.join(format(ord(i), 'b') for i in XORfunc(str1, str2))
     return result_string.count("1")
 
-print HammingDistance("this is a test", "wokka wokka!!!")
+opt_string = b64decode(''.join(open("6.txt", "r")))
+
+normalized_distances = []
+for key_size in range(2, 40):
+    s1 = opt_string[:key_size]
+    s2 = opt_string[key_size:key_size*2]
+    s3 = opt_string[key_size*2:key_size*3]
+    s4 = opt_string[key_size*3:key_size*4]
+
+    normalized_distance = float(HammingDistance(s1, s2) + HammingDistance(s2, s3) + HammingDistance(s3, s4)) / (key_size * 3)
+    normalized_distances.append((key_size, normalized_distance))
+
+normalized_distances = sorted(normalized_distances, key=lambda (_, y): y)
+
+print normalized_distances[0][0]
